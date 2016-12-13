695dfc97df0eb0c946f8aad830e6c4f50d93d759
JK Gunnink <jgunnink@gmail.com>
2016-12-13 16:33:19 +0800

    Create supervisor which starts web worker

--- lib/alchemapi.ex
+++ lib/alchemapi.ex
@@ -1,2 +1,14 @@
-defmodule Alchemapi do
+defmodule Alchemapi.Api do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Alchemapi.Web, [])
+    ]
+
+    opts = [strategy: :one_for_one, name: Alchemapi.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
